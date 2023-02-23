4652ba6ed36a0f4d0206cdc1ba29ef38773a94f3
NunezAN <alex101nunez@gmail.com>
2023-02-23 10:35:51 -0600

    flattened the arr

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   const flattenArr = (array) =>
     array.reduce((arr, cur) => {
       if (Array.isArray(cur)) {
+        // console.log(cur);
         return arr.concat(flattenArr(cur));
       } else {
         return arr.concat(cur);
       }
-    });
+    }, []);
+  // const flattenArr = (arr) =>
+  //   arr.reduce(
+  //     (acc, val) =>
+  //       Array.isArray(val) ? acc.concat(flattenArr(val)) : acc.concat(val),
+  //     []
+  //   );
   console.log(flattenArr(arr));
   return <div className=""></div>;
 }
