28b33d49954625b0b75661953f3f1e5bea5f28b7
NunezAN <alex101nunez@gmail.com>
2023-02-23 10:54:20 -0600

    compose of functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 function App() {
-  const arr = [
-    [1, 2],
-    [3, 4],
-    [5, 6, [7, 8], 9],
-    [10, 11, 12],
-  ];
-  const flattenArr = (array) =>
-    array.reduce((arr, cur) => {
-      if (Array.isArray(cur)) {
-        // console.log(cur);
-        return arr.concat(flattenArr(cur));
-      } else {
-        return arr.concat(cur);
-      }
-    }, []);
-  // const flattenArr = (arr) =>
-  //   arr.reduce(
-  //     (acc, val) =>
-  //       Array.isArray(val) ? acc.concat(flattenArr(val)) : acc.concat(val),
-  //     []
-  //   );
-  console.log(flattenArr(arr));
+  function substractTwo(a) {
+    return a - 2;
+  }
+  function multiplyFour(a) {
+    return a * 4;
+  }
+  const compose = (...functions) => {
+    return (args) =>
+      functions.reduceRight((arg, fn) => {
+        return fn(arg);
+      }, args);
+  };
+  const evaluate = compose(substractTwo, multiplyFour);
+  console.log(evaluate(5)); // 23
+
   return <div className=""></div>;
 }
 
